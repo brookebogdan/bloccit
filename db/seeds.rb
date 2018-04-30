21788671ee65ed75273d28f6cfe60398d2586977
Brooke Bogdan <brkabogdan@gmail.com>
2018-04-29 23:12:39 -0400

    assignment 19 completed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,25 +1,21 @@
 require 'random_data'
 
-# Create Posts
 50.times do
-# #1
   Post.create!(
-# #2
     title:  RandomData.random_sentence,
     body:   RandomData.random_paragraph
   )
 end
 posts = Post.all
 
-# Create Comments
-# #3
 100.times do
   Comment.create!(
-# #4
     post: posts.sample,
     body: RandomData.random_paragraph
   )
 end
+
+Post.find_or_create_by(title: "A unique title", body: "A unique body")
 
 puts "Seed finished"
 puts "#{Post.count} posts created"
