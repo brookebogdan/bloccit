b2a1a1ac916ed61762ff92033495dc412930921b
Brooke Bogdan <brkabogdan@gmail.com>
2018-05-17 21:33:23 -0400

    checkpoint 26 work completed

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180501033614) do
+ActiveRecord::Schema.define(version: 20180515002724) do
 
   create_table "advertisements", force: :cascade do |t|
     t.string   "title"
@@ -20,15 +20,6 @@ ActiveRecord::Schema.define(version: 20180501033614) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
-
-  create_table "answers", force: :cascade do |t|
-    t.integer  "question_id"
-    t.text     "body"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
-  end
-
-  add_index "answers", ["question_id"], name: "index_answers_on_question_id"
 
   create_table "comments", force: :cascade do |t|
     t.text     "body"
@@ -44,7 +35,10 @@ ActiveRecord::Schema.define(version: 20180501033614) do
     t.text     "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "topic_id"
   end
+
+  add_index "posts", ["topic_id"], name: "index_posts_on_topic_id"
 
   create_table "questions", force: :cascade do |t|
     t.string   "title"
@@ -52,6 +46,14 @@ ActiveRecord::Schema.define(version: 20180501033614) do
     t.boolean  "resolved"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "topics", force: :cascade do |t|
+    t.string   "name"
+    t.boolean  "public",      default: true
+    t.text     "description"
+    t.datetime "created_at",                 null: false
+    t.datetime "updated_at",                 null: false
   end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,13 @@
 require 'random_data'
 
+15.times do
+  Topic.create!(
+    name:         RandomData.random_sentence,
+    description:  RandomData.random_paragraph
+  )
+end
+topics = Topic.all
+
 # Create Posts
 50.times do
 # #1
@@ -12,23 +20,23 @@ end
 posts = Post.all
 
 # Create Comments
-# #3
 100.times do
   Comment.create!(
-# #4
     post: posts.sample,
     body: RandomData.random_paragraph
   )
 end
 
-50.times do
-  Advertisement.create!(
-    title: RandomData.random_sentence,
-    copy: RandomData.random_paragraph,
-    price: RandomData.random_integer
-  )
-end
+# 50.times do
+#   Advertisement.create!(
+#     title: RandomData.random_sentence,
+#     copy: RandomData.random_paragraph,
+#     price: RandomData.random_integer
+#   )
+# end
 
 puts "Seed finished"
+puts "#{Topic.count} topics created"
 puts "#{Post.count} posts created"
 puts "#{Comment.count} comments created"
+# puts "#{Advertisement.count} advertisements created"
