c2e3582ef47aec78a96c360b14acffaf76ef9b2d
Brooke Bogdan <brkabogdan@gmail.com>
2018-06-06 15:04:18 -0400

    checkpoint-34-assignment work completed

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
-ActiveRecord::Schema.define(version: 20180521011115) do
-=======
 ActiveRecord::Schema.define(version: 20180531162312) do
->>>>>>> checkpoint-34
 
   create_table "advertisements", force: :cascade do |t|
     t.string   "title"
