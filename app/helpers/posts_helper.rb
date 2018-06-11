dee3b7b6e66944f8a761dbbbc1069d9daad0f7b5
Brooke Bogdan <brkabogdan@gmail.com>
2018-06-11 18:04:15 -0400

    checkpoint 37 completed

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,9 +1,5 @@
 module PostsHelper
-	def user_is_authorized_for_post?(post)
-		current_user && (current_user == post.user || current_user.admin? || current_user.moderator?)
-	end
-
-	def user_is_authorized_for_delete?
-		current_user && (current_user == post.user || current_user.admin?)
-	end
+  def user_is_authorized_for_post?(post)
+     current_user && (current_user == post.user || current_user.admin?)
+  end
 end
